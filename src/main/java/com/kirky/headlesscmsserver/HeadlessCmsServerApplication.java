5d6ddbb6a16eb6fff5efb9365dc1d7b64b72553b
Kirk Santos <kirkalyn13@gmail.com>
2024-04-27 15:09:11 +0800

    initial commit

---
+++ src/main/java/com/kirky/headlesscmsserver/HeadlessCmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.kirky.headlesscmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessCmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlessCmsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirky/headlesscmsserver/HeadlessCmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirky.headlesscmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlessCmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
