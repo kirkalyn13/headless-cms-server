02999185734ca5e7b1988d4228b701e1973b81a8
Kirk Santos <kirkalyn13@gmail.com>
2024-04-27 21:19:08 +0800

    fix: content properties

--- src/main/java/com/kirky/headlesscmsserver/model/Content.java
+++ src/main/java/com/kirky/headlesscmsserver/model/Content.java
@@ -21,8 +21,11 @@ public class Content {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "author")
-    private String subtitle;
+    private String author;
 
     @JsonBackReference
     @OneToMany( fetch = FetchType.LAZY, mappedBy = "id", cascade = CascadeType.ALL)
